7c38e4a1a48b03ccb91c8351fb5f4f631bc7fb3b
Mayakannan C <146314212+mayakannan2002@users.noreply.github.com>
2025-07-18 15:39:43 +0530

    login page compete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Signup from './pages/login&signup'
+
 
 const App = () => {
   return (
@@ -10,6 +12,7 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<></>} />
+        <Route path="/login" element={<Signup/>} />
         <Route path="/profit" element={<></>} />
         <Route path="/make-links" element={<></>} />
         <Route path="/profile" element={<></>} />
---
+++ src/pages/login&signup.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sign_header from './../components/Login&signup/header'
+import Login from '../components/Login&signup/login'
+
+
+
+const AboutUs = () => {
+  return (
+    <div>
+      <Sign_header />
+      <Login />
+
+
+    </div>
+  )
+}
+
+export default AboutUs
