f6e210f7e8a8cf54693c5a1a62fe36966f16e7da
kathir <nskathir12@gmail.com>
2025-07-18 19:07:06 +0530

    commit1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Signup from './pages/login&signup'
+import Makelinks from './pages/MakeLinkPages'
 
 
 const App = () => {
@@ -14,7 +15,7 @@ const App = () => {
         <Route path="/" element={<></>} />
         <Route path="/login" element={<Signup/>} />
         <Route path="/profit" element={<></>} />
-        <Route path="/make-links" element={<></>} />
+        <Route path="/make-links" element={<Makelinks/>} />
         <Route path="/profile" element={<></>} />
       </Routes>
 
