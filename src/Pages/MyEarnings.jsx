e3afd50ccbe739eb64fae1f07074ce6c5955d51a
kathir <nskathir12@gmail.com>
2025-07-23 15:48:59 +0530

    overall 2

--- src/Pages/MyEarnings.jsx
+++ src/Pages/MyEarnings.jsx
@@ -1,6 +1,9 @@
 // src/pages/MakeLinkPage.jsx
 import React from "react";
 import Earnings from "../components/Makelinks/Myearnings/Earning";
+import Order from "./../components/Makelinks/Myearnings/order";
+import Report from "./../components/Makelinks/Myearnings/report";
+import Total_Earnings from "./../components/Makelinks/Myearnings/total_earning";
 
 
 export default function MakeLinkPage() {
